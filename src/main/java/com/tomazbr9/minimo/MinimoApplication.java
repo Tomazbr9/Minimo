f4656027d45a536639eaccfa1f1e38b0bf7bffad
tomaz <tomazcraft@gmail.com>
2025-09-07 22:33:37 -0300

    chore: iniciliaza projeto com estrutura de pastas

---
+++ src/main/java/com/tomazbr9/minimo/MinimoApplication.java
@@ -0,0 +1,13 @@
+package com.tomazbr9.minimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomazbr9/minimo/MinimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomazbr9.minimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
